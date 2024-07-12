cfd9475e946ec72f850dae2cd5dcbd0785a97a30
viki <viki@qq.com>
2024-07-12 17:37:47 +0800

    双击编辑裂变，还没存数据库

--- src/renderer/src/components/List.tsx
+++ src/renderer/src/components/List.tsx
@@ -1,14 +1,24 @@
-import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react';
-import { Table } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
+import React, { forwardRef, useContext, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { Form, Input, Table } from 'antd';
+import type { FormInstance, InputRef, TableColumnsType, TableProps } from 'antd';
 
 type TableRowSelection<T> = TableProps<T>['rowSelection'];
+const EditableContext = React.createContext<FormInstance<any> | null>(null);
 
 interface DataType {
   key: React.Key;
   name: string;
   age: number;
   address: string;
+}
+
+
+interface EditableCellProps {
+  title: React.ReactNode;
+  editable: boolean;
+  dataIndex: any;
+  record: any;
+  handleSave: (record: any) => void;
 }
 
 // const columns: TableColumnsType<DataType> = [
@@ -38,6 +48,11 @@ interface DataType {
 const scroll = {
   x: '100vw', y: 240
 }
+
+const currentData = {
+  rows: [],
+  columns: []
+}
 const DataList: React.FC = (props, parentRef) => {
   const inputRef = useRef(null);
 
@@ -45,28 +60,45 @@ const DataList: React.FC = (props, parentRef) => {
   const [data, setData] = useState<React.Key[]>([]);
   const [columns, setColumns] = useState<React.Key[]>([]);
 
+  const handleSave = (row: any) => {
+    const newData = [...currentData.rows];
+    console.log('handleSave row: ', row)
+    console.log('handleSave newData: ', currentData.rows, data)
+    const index = newData.findIndex((item) => row.id === item.id);
+    const item = newData[index];
+    console.log('handleSave item: ', item, index)
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    setData(newData);
+  };
+
   useImperativeHandle(parentRef, () => {
     return {
       updateList (listData) {
-        let rows = listData.rows.map((el, index) => {
-          // el.key = 
-          return {
-            key: index,
-            name: el.user_name || el.name,
-            address: el.id,
-            age: el.id
-          }
 
-        })
+        console.log('useImperativeHandle data: ', data)
+
         listData.rows.forEach(el => el.key = `${new Date().getTime()}_${(Math.random() + '').replace('.', '')}`)
 
         console.log('column rows: ', listData.columns, listData.rows)
         setData(listData.rows)
+        currentData.rows = listData.rows
+
+        console.log('update data:', data)
 
         setColumns(listData.columns.map(el => {
           return {
             title: el.column_name,
             dataIndex: el.column_name,
+            onCell: (record: DataType) => ({
+              record,
+              editable: true,
+              dataIndex: el.column_name,
+              title: el.column_name,
+              handleSave,
+            })
           }
         }))
       }
@@ -114,7 +146,94 @@ const DataList: React.FC = (props, parentRef) => {
     ],
   };
 
-  return <Table scroll={{ x: 'max-content' }} rowSelection={rowSelection} columns={columns} dataSource={data} ref={inputRef} />;
+
+  interface EditableRowProps {
+    index: number;
+  }
+
+  const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...props} />
+        </EditableContext.Provider>
+      </Form>
+    );
+  };
+
+
+  const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef<InputRef>(null);
+    const form = useContext(EditableContext)!;
+
+    useEffect(() => {
+      if (editing) {
+        inputRef.current?.focus();
+      }
+    }, [editing]);
+
+    const toggleEdit = () => {
+      setEditing(!editing);
+      form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+    };
+
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+
+        console.log('values: ', values)
+
+        toggleEdit();
+        handleSave({ ...record, ...values });
+      } catch (errInfo) {
+        console.log('Save failed:', errInfo);
+      }
+    };
+
+    let childNode = children;
+
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{ margin: 0 }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+        </Form.Item>
+      ) : (
+        <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onDoubleClick={toggleEdit}>
+          {children}
+        </div>
+      );
+    }
+
+    return <td {...restProps}>{childNode}</td>;
+  }
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+
+  return <Table scroll={{ x: 'max-content' }} components={components} rowSelection={rowSelection} columns={columns} dataSource={data} ref={inputRef} />;
 };
 
 // export default DataList;
