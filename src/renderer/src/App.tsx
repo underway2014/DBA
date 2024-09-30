55a449608c599e30266937332a56f7c5a7f0a342
viki <viki@qq.com>
2024-09-30 10:14:29 +0800

    change version

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,10 @@
-import { ConfigProvider, theme } from 'antd'
 import CLayout from './components/Layout'
 
 function App(): JSX.Element {
   return (
-    // <ConfigProvider theme={{ algorithm: undefined }}>
     <div>
       <CLayout></CLayout>
     </div>
-    // </ConfigProvider>
   )
 }
 
