94483e420453e489bd617c1479d75862d11b2cf6
viki <viki@qq.com>
2025-12-09 16:57:50 +0800

    版本修改

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,39 +1,39 @@
-import Koa from 'koa'
+// import Koa from 'koa'
 
-import bodyParse from 'koa-bodyparser'
+// import bodyParse from 'koa-bodyparser'
 
-import Router from '@koa/router'
-// import { getDb } from './db';
-const router = new Router()
+// import Router from '@koa/router'
+// // import { getDb } from './db';
+// const router = new Router()
 
-async function server() {
-  const app: Koa = new Koa()
+// async function server() {
+//   const app: Koa = new Koa()
 
-  app.use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Credentials', 'true')
-    ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-    ctx.set('Access-Control-Allow-Origin', '*')
-    await next()
-  })
+//   app.use(async (ctx, next) => {
+//     ctx.set('Access-Control-Allow-Credentials', 'true')
+//     ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//     ctx.set('Access-Control-Allow-Origin', '*')
+//     await next()
+//   })
 
-  app.use(bodyParse())
+//   app.use(bodyParse())
 
-  // const db = getDb({name: 'local-server', config: {}})
+//   // const db = getDb({name: 'local-server', config: {}})
 
-  router.get('/list', async (ctx, _) => {
-    const sql = `
-        select * from active limit 2
-        `
+//   router.get('/list', async (ctx, _) => {
+//     const sql = `
+//         select * from active limit 2
+//         `
 
-    // let result = await db.query(sql, {type: QueryTypes.SELECT})
+//     // let result = await db.query(sql, {type: QueryTypes.SELECT})
 
-    ctx.status = 200
-    ctx.body = { data: sql }
-  })
+//     ctx.status = 200
+//     ctx.body = { data: sql }
+//   })
 
-  app.use(router.routes())
+//   app.use(router.routes())
 
-  app.listen(3000)
-}
+//   app.listen(3000)
+// }
 
-export default server
+// export default server
